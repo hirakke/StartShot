9c91bf82c83490e3638a78e2f09011e287a7c882
Keiju Hiramoto <hiramoto.keiju@gmail.com>
2026-03-05 16:59:14 +0900

    作成

---
+++ StartShot/CameraView.swift
@@ -0,0 +1,18 @@
+//
+//  CameraView.swift
+//  StartShot
+//
+//  Created by Keiju Hiramoto on 2026/03/05.
+//
+
+import SwiftUI
+
+struct CameraView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CameraView()
+}
---
+++ StartShot/TabView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  StartShot
+//
+//  Created by Keiju Hiramoto on 2026/03/05.
+//
+
+import SwiftUI
+
+struct TabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TabView()
+}
