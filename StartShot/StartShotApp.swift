eab9a147a4b8c04e7f6cf1cb6aa81d249a9b1cd1
Keiju Hiramoto <hiramoto.keiju@gmail.com>
2026-03-05 16:23:44 +0900

    Initial Commit

---
+++ StartShot/StartShotApp.swift
@@ -0,0 +1,32 @@
+//
+//  StartShotApp.swift
+//  StartShot
+//
+//  Created by Keiju Hiramoto on 2026/03/05.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct StartShotApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
